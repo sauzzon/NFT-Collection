7d02b90da5c3763f1b890a723050f7592dd408d6
Saujan Tiwari <saujan.tiwari@gmail.com>
2022-05-18 22:20:40 +0545

    NFT dapp

---
+++ hardhat/constants/index.js
@@ -0,0 +1,3 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xe22df6f2112f6b954be64757eefae7c22bbdfff4";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
